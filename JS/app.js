aa770851931f038bd0c1ee108125af98a63d3687
daudi13 <davouma13@gmail.com>
2021-04-28 06:49:47 +0300

    commit transition

--- JS/app.js
+++ JS/app.js
@@ -2,7 +2,8 @@ function imgSlider(anything) {
     document.querySelector('.coffee').src = anything;
 }
 
-function changeArcColor(color) {
+function changeArcColor(color, transition) {
     const theArc = document.querySelector('.arcs');
     theArc.style.background = color;
+    theArc.style.transition = transition
 } 
