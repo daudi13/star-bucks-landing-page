068ef06f1d641ea7738592d1bbe577c86a0eb725
daudi13 <davouma13@gmail.com>
2021-04-28 06:13:25 +0300

    add js

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+    document.querySelector('.coffee').src = anything;
+}
+
+function changeArcColor(color) {
+    const theArc = document.querySelector('.arcs');
+    theArc.style.background = color;
+} 
