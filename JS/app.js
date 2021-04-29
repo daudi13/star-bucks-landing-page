167863dbf099910d8b4f6869776c6201f046f3ae
daudi13 <davouma13@gmail.com>
2021-04-29 04:19:24 +0300

    commit changes

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,10 @@
 function imgSlider(anything) {
     document.querySelector('.coffee').src = anything;
+    const coffee = document.querySelector('.coffee');
 }
 
 function changeArcColor(color, transition) {
     const theArc = document.querySelector('.arcs');
     theArc.style.background = color;
-    theArc.style.transition = transition
+    theArc.style.transition = transition;
 } 
